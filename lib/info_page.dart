1dc2d471d47b2fd3925ed6c8e1f0aea6519a5801
Robodozer <vijay@ashokn.org>
2022-07-02 09:45:48 -0400

    edited font size

--- lib/info_page.dart
+++ lib/info_page.dart
@@ -6,41 +6,42 @@ class InfoPage extends StatelessWidget {
   Widget infoText() {
     return RichText(
       text: TextSpan(
+        style: const TextStyle(fontSize: 30),
         children: [
           const TextSpan(
-            text: "'HOW TO PLAY: First, press the ",
+            text: "HOW TO PLAY: First, press the ",
           ),
           const WidgetSpan(
-            child: Icon(Icons.auto_fix_normal, color: Colors.blue),
+            child: Icon(Icons.auto_fix_normal, color: Colors.blue, size: 40),
           ),
           const TextSpan(
             text:
                 " at the bottom right of the card you want to play. If you want to cancel playing that card, just press the ",
           ),
           const WidgetSpan(
-            child: Icon(Icons.undo, color: Colors.blue),
+            child: Icon(Icons.undo, color: Colors.blue, size: 40),
+          ),
+          const TextSpan(
+            text: " at the bottom right of the selected card. Then, press the ",
+          ),
+          const WidgetSpan(
+            child: Icon(Icons.ads_click, color: Colors.red, size: 40),
           ),
           const TextSpan(
             text:
-                " at the bottom right of the selected card. Then, press the  ",
+                " at the bottom right of the card you want to attack. If you want to cancel attacking that card, just press the ",
           ),
           const WidgetSpan(
-            child: Icon(Icons.ads_click, color: Colors.red),
-          ),
-          const TextSpan(
-            text:
-                "at the bottom right of the card you want to attack. If you want to cancel attacking that card, just press the ",
-          ),
-          const WidgetSpan(
-            child: Icon(Icons.undo, color: Colors.orange),
+            child: Icon(Icons.undo, color: Colors.orange, size: 40),
           ),
           const TextSpan(
             text: ". If you want to target that card, just press the ",
           ),
           WidgetSpan(
               child: FloatingActionButton.extended(
-            icon: const Icon(Icons.play_arrow),
+            icon: const Icon(Icons.play_arrow, size: 20),
             label: const Text("Play Turn"),
+            extendedTextStyle: const TextStyle(fontSize: 15),
             onPressed: () {},
           )),
         ],
